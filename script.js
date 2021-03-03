fcc27c26bca476c56a9d0995272cdc831c490083
dhanshri96 <79906045+dhanshri96@users.noreply.github.com>
2021-03-03 13:07:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+            {
+                p = document.getElementById("principal").value;
+                n = document.getElementById("rate").value;
+                r = document.getElementById("years").value;
+                result = document.getElementById("result");
+                
+                result.innerHTML = "If you deposit principal='' at an interest of rate=''.You will receive an amount result='',in the year years='' " + (p*n*r/100);
+            }
+
+ 

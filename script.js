8404ffcc56c50c1d1a9440257f3f1ff05751bba8
dhanshri96 <dhanshri96@gmail.com>
2021-03-07 07:44:43 +0000

    My Simple interest calculator Prep

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 function compute()
-            {
-                p = document.getElementById("principal").value;
-                n = document.getElementById("rate").value;
-                r = document.getElementById("years").value;
-                result = document.getElementById("result");
-                
-                result.innerHTML = "If you deposit principal='' at an interest of rate=''.You will receive an amount result='',in the year years='' " + (p*n*r/100);
-            }
-
- 
+{
+    // Get input values
+    var principal = parseFloat( document.getElementById("principal").value );
+    var rate = parseFloat( document.getElementById("rate").value );
+    var years = parseFloat( document.getElementById("years").value );
+    // Validate the values are good
+    if (principal <= 0 || isNaN(principal)){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return
+    }
+    // Calculate the total interest
+    var interest = principal * (  ( years * rate / 100 ) )
+    // Calculatet the final year
+    var today = new Date()
+    var year = today.getFullYear() + years;
+    // Set the message
+    var result = "If you deposit <mark>" + principal + "</mark><br>"
+    result += "at an interest rate of <mark>" + rate  + "</mark>.<br>"
+    result += "You will receive an amount of <mark>" + interest + "</mark>,<br>"
+    result += "in the year <mark>" + year + "</mark>."
+    document.getElementById("result").innerHTML  = result;
+    // Reset focus to principal element
+    document.getElementById("principal").focus(); 
+}
+        
